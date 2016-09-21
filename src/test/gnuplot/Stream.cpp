e30cc540c179f12d67ef90c2fa7733605dd1c9a0
Geert Fannes <geert.fannes@gmail.com>
2016-09-21 02:10:09 +0200

    test uses column naming now

--- src/test/gnuplot/Stream.cpp
+++ src/test/gnuplot/Stream.cpp
@@ -6,7 +6,10 @@ TEST_CASE("gnuplot tests", "[ut][gnuplot]")
 {
     gubg::gnuplot::Stream gs;
 
-    gs.name(0, "sinus").name(1, "cosinus");
+    gs.name(0, "sinus data");
+    gs.name(0, 0, "x").name(0, 1, "sinus(x)");
+    gs.name(1, "cosinus data");
+    gs.name(1, 0, "x").name(1, 1, "cosinus(x)");
     for (double t = -3.0; t <= 3.0; t += 0.01)
     {
         gs.data() << t << std::sin(t);
@@ -14,7 +17,8 @@ TEST_CASE("gnuplot tests", "[ut][gnuplot]")
     }
 
     gs.pause();
-    gs.name(42, "sinc");
+    gs.name(42, "the answer");
+    gs.name(42, 0, "x").name(42, 1, "round(sin(x))").name(42, 2, "sin(round(x))");
     for (double t = -10.0; t <= 10.0; t += 0.1)
         gs.data(42) << t << std::round(std::sin(t)) << std::sin(std::round(t));
 
