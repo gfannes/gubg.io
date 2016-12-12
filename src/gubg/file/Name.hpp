6800c5ecea46c22c74c739c4b2d3640f4e10fed7
Geert Fannes <geert.fannes@gmail.com>
2016-12-12 22:16:03 +0100

    added file::Name, a makeover of file::File

---
+++ src/gubg/file/Name.hpp
@@ -0,0 +1,33 @@
+#ifndef HEADER_gubg_file_Name_hpp_ALREADY_INCLUDED
+#define HEADER_gubg_file_Name_hpp_ALREADY_INCLUDED
+
+#include <string>
+#include <ostream>
+
+namespace gubg { namespace file { 
+
+    class Name
+    {
+        public:
+            Name() = default;
+            Name(const std::string &name): name_(name) {}
+            Name(const char *name): name_(name) {}
+
+            void stream(std::ostream &os) const
+            {
+                os << name_;
+            }
+
+        private:
+            std::string name_;
+    };
+
+    inline std::ostream &operator<<(std::ostream &os, const Name &name)
+    {
+        name.stream(os);
+        return os;
+    }
+
+} } 
+
+#endif
