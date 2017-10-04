bb850b10ed83ab819ed9bbcecd8192a3fe16b891
Geert Fannes <geert.fannes@auro-technologies.com>
2017-10-04 22:13:20 +0200

    indent

--- src/gubg/Strange.cpp
+++ src/gubg/Strange.cpp
@@ -4,47 +4,47 @@
 
 namespace gubg { 
 
-	Strange::Strange():
+    Strange::Strange():
         s_(0), l_(0)
     {
     }
-	Strange::Strange(const std::string &str)
-        :s_(str.data()), l_(str.size())
+    Strange::Strange(const std::string &str)
+    :s_(str.data()), l_(str.size())
     {
     }
 
-	bool Strange::empty() const
+    bool Strange::empty() const
     {
         return l_ == 0;
     }
-	size_t Strange::size() const
+    size_t Strange::size() const
     {
         return l_;
     }
-	std::string Strange::str() const
+    std::string Strange::str() const
     {
         return std::string(s_, l_);
     }
-	char Strange::front() const
+    char Strange::front() const
     {
         assert(s_ && l_);
         return *s_;
     }
-	char Strange::back() const
+    char Strange::back() const
     {
         assert(s_ && l_);
         return s_[l_-1];
     }
-	void Strange::clear()
+    void Strange::clear()
     {
         s_ = 0;
         l_ = 0;
     }
 
-	bool Strange::contains(char ch) const
-	{
-		return nullptr != std::memchr(s_, ch, l_);
-	}
+    bool Strange::contains(char ch) const
+    {
+        return nullptr != std::memchr(s_, ch, l_);
+    }
 
     bool Strange::strip(char ch)
     {
@@ -107,203 +107,203 @@ namespace gubg {
         return true;
     }
 
-	bool Strange::pop_all(Strange &res)
-	{
-		assert(invariants_());
-		res = *this;
-		clear();
+    bool Strange::pop_all(Strange &res)
+    {
+        assert(invariants_());
+        res = *this;
+        clear();
         return !res.empty();
-	}
-	bool Strange::pop_all(std::string &res)
-	{
-		assert(invariants_());
-		Strange s;
-		pop_all(s);
-		res = s.str();
+    }
+    bool Strange::pop_all(std::string &res)
+    {
+        assert(invariants_());
+        Strange s;
+        pop_all(s);
+        res = s.str();
         return !res.empty();
-	}
-	//Does not pop ch
-	bool Strange::pop_to(Strange &res, const char ch)
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		char *ptr = (char*)std::memchr(s_, ch, l_);
-		if (!ptr)
-			return false;
-		res.s_ = s_;
-		res.l_ = ptr-s_;
-		forward_(res.l_);
-		return true;
-	}
-	//Does not pop str
-	bool Strange::pop_to(Strange &res, const std::string &str)
-	{
-		assert(invariants_());
-		if (str.empty())
-			return false;
-		//We will iteratively search for ch, and try to match the rest of str
-		const char ch = str[0];
-		const size_t s = str.size();
-		const Strange sp = *this;
-		while (char *ptr = (char*)std::memchr(s_, ch, l_))
-		{
-			const size_t l = ptr-s_;
-			forward_(l);
-			if (size() < s)
-				break;
-			if (0 == std::memcmp(str.data(), s_, s))
-			{
-				res.s_ = sp.s_;
-				res.l_ = s_-res.s_;
-				return true;
-			}
-		}
-		*this = sp;
-		return false;
-	}
-	bool Strange::diff_to(const Strange &strange)
-	{
+    }
+    //Does not pop ch
+    bool Strange::pop_to(Strange &res, const char ch)
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        char *ptr = (char*)std::memchr(s_, ch, l_);
+        if (!ptr)
+            return false;
+        res.s_ = s_;
+        res.l_ = ptr-s_;
+        forward_(res.l_);
+        return true;
+    }
+    //Does not pop str
+    bool Strange::pop_to(Strange &res, const std::string &str)
+    {
+        assert(invariants_());
+        if (str.empty())
+            return false;
+        //We will iteratively search for ch, and try to match the rest of str
+        const char ch = str[0];
+        const size_t s = str.size();
+        const Strange sp = *this;
+        while (char *ptr = (char*)std::memchr(s_, ch, l_))
+        {
+            const size_t l = ptr-s_;
+            forward_(l);
+            if (size() < s)
+                break;
+            if (0 == std::memcmp(str.data(), s_, s))
+            {
+                res.s_ = sp.s_;
+                res.l_ = s_-res.s_;
+                return true;
+            }
+        }
+        *this = sp;
+        return false;
+    }
+    bool Strange::diff_to(const Strange &strange)
+    {
         if (empty())
             //Already at the end
-			return false;
+            return false;
         if (strange.empty())
             //If strange is empty, we cannot trust its s_. We assume strange ran to its end and will return everything in res.
             return true;
-		if (strange.s_ < s_)
-			//Not really expected
-			return false;
+        if (strange.s_ < s_)
+            //Not really expected
+            return false;
         l_ = (strange.s_ - s_);
         return true;
-	}
-	//Pops ch too, set inclusive to true if you want ch to be included in res
-	bool Strange::pop_until(Strange &res, const char ch, bool inclusive)
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		for (size_t i = 0; i < l_; ++i)
-			if (s_[i] == ch)
-			{
-				res.s_ = s_;
-				res.l_ = i + (inclusive ? 1 : 0);
-				forward_(i+1);
-				return true;
-			}
+    }
+    //Pops ch too, set inclusive to true if you want ch to be included in res
+    bool Strange::pop_until(Strange &res, const char ch, bool inclusive)
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        for (size_t i = 0; i < l_; ++i)
+            if (s_[i] == ch)
+            {
+                res.s_ = s_;
+                res.l_ = i + (inclusive ? 1 : 0);
+                forward_(i+1);
+                return true;
+            }
 
-		return false;
-	}
-	bool Strange::pop_until(std::string &res, const char ch, bool inclusive)
-	{
-		Strange s;
-		if (!pop_until(s, ch, inclusive))
-			return false;
-		res = s.str();
-		return true;
-	}
-	bool Strange::pop_until(Strange &res, const std::string &str, bool inclusive)
-	{
-		assert(invariants_());
-		if (str.empty())
-			return true;
-		const size_t s = str.size();
-		if (size() < s)
-			//We are to small to match str
-			return false;
-		const auto ch = str.front();
-		const auto l2check = l_-s+1;
-		for (size_t i = 0; i < l2check; ++i)
-			if (s_[i] == ch)
-			{
-				//Pontential match, check the rest of str
-				if (!std::memcmp(str.data(), s_+i, s))
-				{
-					res.s_ = s_;
-					res.l_ = i + (inclusive ? s : 0);
-					forward_(i+s);
-					return true;
-				}
-			}
-		return false;
-	}
-	bool Strange::pop_until(std::string &res, const std::string &str, bool inclusive)
-	{
-		Strange s;
-		if (!pop_until(s, str, inclusive))
-			return false;
-		res = s.str();
-		return true;
-	}
-	bool Strange::pop_decimal(long &res)
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		size_t l = l_;
-		if (!parse::numbers::read(res, s_, l))
-			return false;
-		forward_(l);
-		return true;
-	}
-	bool Strange::pop_float(double &res)
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		char *e = 0;
-		double d = std::strtod(s_, &e);
-		if (e == s_)
-			return false;
-		res = d;
-		forward_(e-s_);
-		return true;
-	}
+        return false;
+    }
+    bool Strange::pop_until(std::string &res, const char ch, bool inclusive)
+    {
+        Strange s;
+        if (!pop_until(s, ch, inclusive))
+            return false;
+        res = s.str();
+        return true;
+    }
+    bool Strange::pop_until(Strange &res, const std::string &str, bool inclusive)
+    {
+        assert(invariants_());
+        if (str.empty())
+            return true;
+        const size_t s = str.size();
+        if (size() < s)
+            //We are to small to match str
+            return false;
+        const auto ch = str.front();
+        const auto l2check = l_-s+1;
+        for (size_t i = 0; i < l2check; ++i)
+            if (s_[i] == ch)
+            {
+                //Pontential match, check the rest of str
+                if (!std::memcmp(str.data(), s_+i, s))
+                {
+                    res.s_ = s_;
+                    res.l_ = i + (inclusive ? s : 0);
+                    forward_(i+s);
+                    return true;
+                }
+            }
+        return false;
+    }
+    bool Strange::pop_until(std::string &res, const std::string &str, bool inclusive)
+    {
+        Strange s;
+        if (!pop_until(s, str, inclusive))
+            return false;
+        res = s.str();
+        return true;
+    }
+    bool Strange::pop_decimal(long &res)
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        size_t l = l_;
+        if (!parse::numbers::read(res, s_, l))
+            return false;
+        forward_(l);
+        return true;
+    }
+    bool Strange::pop_float(double &res)
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        char *e = 0;
+        double d = std::strtod(s_, &e);
+        if (e == s_)
+            return false;
+        res = d;
+        forward_(e-s_);
+        return true;
+    }
 
-	bool Strange::pop_if(const char ch)
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		if (*s_ != ch)
-			return false;
-		forward_(1);
-		return true;
-	}
-	bool Strange::pop_back_if(const char ch)
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		if (s_[l_-1] != ch)
-			return false;
-		shrink_(1);
-		return true;
-	}
-	bool Strange::pop_front()
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		forward_(1);
-		return true;
-	}
-	bool Strange::pop_back()
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		shrink_(1);
-		return true;
-	}
-	bool Strange::pop_char(char &ch)
-	{
-		assert(invariants_());
-		if (empty())
-			return false;
-		ch = *s_;
-		forward_(1);
-		return true;
-	}
+    bool Strange::pop_if(const char ch)
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        if (*s_ != ch)
+            return false;
+        forward_(1);
+        return true;
+    }
+    bool Strange::pop_back_if(const char ch)
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        if (s_[l_-1] != ch)
+            return false;
+        shrink_(1);
+        return true;
+    }
+    bool Strange::pop_front()
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        forward_(1);
+        return true;
+    }
+    bool Strange::pop_back()
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        shrink_(1);
+        return true;
+    }
+    bool Strange::pop_char(char &ch)
+    {
+        assert(invariants_());
+        if (empty())
+            return false;
+        ch = *s_;
+        forward_(1);
+        return true;
+    }
 
     bool Strange::pop_string(std::string &str, size_t nr)
     {
@@ -382,21 +382,21 @@ namespace gubg {
     }
 
     template <typename T>
-        bool Strange::pop_lsb_(T &v)
+    bool Strange::pop_lsb_(T &v)
+    {
+        assert(invariants_());
+        if (l_ < sizeof(v))
+            return false;
+        v = 0;
+        for (int i = 0; i < sizeof(v); ++i)
         {
-            assert(invariants_());
-            if (l_ < sizeof(v))
-                return false;
-            v = 0;
-            for (int i = 0; i < sizeof(v); ++i)
-            {
-                T tmp = *(const T *)(s_+i);
-                tmp <<= i*8;
-                v |= tmp;
-            }
-            forward_(sizeof(v));
-            return true;
+            T tmp = *(const T *)(s_+i);
+            tmp <<= i*8;
+            v |= tmp;
         }
+        forward_(sizeof(v));
+        return true;
+    }
     bool Strange::pop_lsb(std::uint8_t &v)  { return pop_lsb_(v); }
     bool Strange::pop_lsb(std::uint16_t &v) { return pop_lsb_(v); }
     bool Strange::pop_lsb(std::uint32_t &v) { return pop_lsb_(v); }
@@ -407,20 +407,20 @@ namespace gubg {
     bool Strange::pop_lsb(std::int64_t &v)  { return pop_lsb_(v); }
 
     template <typename T>
-        bool Strange::pop_msb_(T &v)
+    bool Strange::pop_msb_(T &v)
+    {
+        assert(invariants_());
+        if (l_ < sizeof(v))
+            return false;
+        v = 0;
+        for (int i = 0; i < sizeof(v); ++i)
         {
-            assert(invariants_());
-            if (l_ < sizeof(v))
-                return false;
-            v = 0;
-            for (int i = 0; i < sizeof(v); ++i)
-            {
-                v <<= 8;
-                v |= *(std::uint8_t*)(s_+i);
-            }
-            forward_(sizeof(v));
-            return true;
+            v <<= 8;
+            v |= *(std::uint8_t*)(s_+i);
         }
+        forward_(sizeof(v));
+        return true;
+    }
     bool Strange::pop_msb(std::uint8_t &v)  { return pop_msb_(v); }
     bool Strange::pop_msb(std::uint16_t &v) { return pop_msb_(v); }
     bool Strange::pop_msb(std::uint32_t &v) { return pop_msb_(v); }
--- src/gubg/Strange.hpp
+++ src/gubg/Strange.hpp
@@ -15,101 +15,101 @@ namespace gubg {
 
     class Strange
     {
-        public:
-            Strange();
-            //Make sure str outlives Strange
-            Strange(const std::string &);
+    public:
+        Strange();
+        //Make sure str outlives Strange
+        Strange(const std::string &);
 
-            bool empty() const;
-            size_t size() const;
-            std::string str() const;
-            char front() const;
-            char back() const;
-            void clear();
+        bool empty() const;
+        size_t size() const;
+        std::string str() const;
+        char front() const;
+        char back() const;
+        void clear();
 
-            bool contains(char ch) const;
+        bool contains(char ch) const;
 
-            bool strip(char ch);
-            bool strip(const std::string &chars);
+        bool strip(char ch);
+        bool strip(const std::string &chars);
 
-            //Return true if !res.empty()
-            bool pop_all(Strange &res);
-            bool pop_all(std::string &res);
+        //Return true if !res.empty()
+        bool pop_all(Strange &res);
+        bool pop_all(std::string &res);
 
-            //Does not pop ch or str
-            bool pop_to(Strange &res, const char ch);
-            bool pop_to(Strange &res, const std::string &str);
-            //Pops ch too, set inclusive to true if you want ch to be included in res
-            bool pop_until(Strange &res, const char ch, bool inclusive = false);
-            bool pop_until(std::string &res, const char ch, bool inclusive = false);
-            bool pop_until(Strange &res, const std::string &str, bool inclusive = false);
-            bool pop_until(std::string &res, const std::string &str, bool inclusive = false);
+        //Does not pop ch or str
+        bool pop_to(Strange &res, const char ch);
+        bool pop_to(Strange &res, const std::string &str);
+        //Pops ch too, set inclusive to true if you want ch to be included in res
+        bool pop_until(Strange &res, const char ch, bool inclusive = false);
+        bool pop_until(std::string &res, const char ch, bool inclusive = false);
+        bool pop_until(Strange &res, const std::string &str, bool inclusive = false);
+        bool pop_until(std::string &res, const std::string &str, bool inclusive = false);
 
-            bool pop_bracket(Strange &res, const std::string &oc);
-            bool pop_bracket(std::string &res, const std::string &oc);
+        bool pop_bracket(Strange &res, const std::string &oc);
+        bool pop_bracket(std::string &res, const std::string &oc);
 
-            //this and strange are assumed to be related and have the same end
-            bool diff_to(const Strange &strange);
+        //this and strange are assumed to be related and have the same end
+        bool diff_to(const Strange &strange);
 
-            bool pop_decimal(long &res);
-            template <typename Int>
-                bool pop_decimal(Int &i)
-                {
-                    long l;
-                    if (!pop_decimal(l))
-                        return false;
-                    i = l;
-                    return true;
-                }
-            bool pop_float(double &res);
+        bool pop_decimal(long &res);
+        template <typename Int>
+        bool pop_decimal(Int &i)
+        {
+            long l;
+            if (!pop_decimal(l))
+                return false;
+            i = l;
+            return true;
+        }
+        bool pop_float(double &res);
 
-            bool pop_if(const char ch);
-            bool pop_back_if(const char ch);
-            bool pop_front();
-            bool pop_back();
-            bool pop_char(char &ch);
+        bool pop_if(const char ch);
+        bool pop_back_if(const char ch);
+        bool pop_front();
+        bool pop_back();
+        bool pop_char(char &ch);
 
-            bool pop_string(std::string &, size_t nr);
-            bool pop_if(const std::string &);
+        bool pop_string(std::string &, size_t nr);
+        bool pop_if(const std::string &);
 
-            bool pop_line(Strange &line);
+        bool pop_line(Strange &line);
 
-            void ltrim(const char ch);
+        void ltrim(const char ch);
 
-            bool pop_lsb(std::uint8_t &);
-            bool pop_lsb(std::uint16_t &);
-            bool pop_lsb(std::uint32_t &);
-            bool pop_lsb(std::uint64_t &);
-            bool pop_lsb(std::int8_t &);
-            bool pop_lsb(std::int16_t &);
-            bool pop_lsb(std::int32_t &);
-            bool pop_lsb(std::int64_t &);
+        bool pop_lsb(std::uint8_t &);
+        bool pop_lsb(std::uint16_t &);
+        bool pop_lsb(std::uint32_t &);
+        bool pop_lsb(std::uint64_t &);
+        bool pop_lsb(std::int8_t &);
+        bool pop_lsb(std::int16_t &);
+        bool pop_lsb(std::int32_t &);
+        bool pop_lsb(std::int64_t &);
 
-            bool pop_msb(std::uint8_t &);
-            bool pop_msb(std::uint16_t &);
-            bool pop_msb(std::uint32_t &);
-            bool pop_msb(std::uint64_t &);
-            bool pop_msb(std::int8_t &);
-            bool pop_msb(std::int16_t &);
-            bool pop_msb(std::int32_t &);
-            bool pop_msb(std::int64_t &);
+        bool pop_msb(std::uint8_t &);
+        bool pop_msb(std::uint16_t &);
+        bool pop_msb(std::uint32_t &);
+        bool pop_msb(std::uint64_t &);
+        bool pop_msb(std::int8_t &);
+        bool pop_msb(std::int16_t &);
+        bool pop_msb(std::int32_t &);
+        bool pop_msb(std::int64_t &);
 
-            bool pop_count(size_t nr);
-            bool pop_count(Strange &, size_t nr);
+        bool pop_count(size_t nr);
+        bool pop_count(Strange &, size_t nr);
 
-            bool pop_raw(char *dst, size_t nr);
+        bool pop_raw(char *dst, size_t nr);
 
-        private:
-            template <typename T>
-                bool pop_lsb_(T &);
-            template <typename T>
-                bool pop_msb_(T &);
-            bool invariants_() const;
-            void forward_(const size_t nr);
-            void shrink_(const size_t nr);
+    private:
+        template <typename T>
+        bool pop_lsb_(T &);
+        template <typename T>
+        bool pop_msb_(T &);
+        bool invariants_() const;
+        void forward_(const size_t nr);
+        void shrink_(const size_t nr);
 
-            const char *s_;
-            size_t l_;
+        const char *s_;
+        size_t l_;
     };
 
     inline std::ostream &operator<<(std::ostream &os, const Strange &strange)
