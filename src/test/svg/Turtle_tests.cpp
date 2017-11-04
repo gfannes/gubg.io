8c59e60f3692d404d88dd761cca187589cd578c1
Geert Fannes <geert.fannes@auro-technologies.com>
2017-11-04 20:12:46 +0100

    Updated the turtle ut

--- src/test/svg/Turtle_tests.cpp
+++ src/test/svg/Turtle_tests.cpp
@@ -8,19 +8,21 @@ TEST_CASE("svg::Turtle", "[ut][turtle]")
 
     t.down();
 
+    const double h = 8;
+    const double w = 20;
+    const double ww = w/2;
+    const double s = 8;
+    const double d = 8;
+    const unsigned int nr = 9;
+    const unsigned int hnrw = 2;
+    const double l = 300;
+
     auto side = [&](bool b)
     {
         const double x = 0;
         const double y = 0 + b*300;
 
-        const double h = 8;
-        const double w = 20;
-        const double s = 8;
-        const double d = 8;
-        const unsigned int nr = 9;
-        const unsigned int hnrw = 1;
-
-        const double offset = (b ? -s : 0);
+        const double offset = (b ? -s : s/2);
 
         auto ripple_a = [&](double delta = 0.0){
             t.color("red").run(w+delta);
@@ -42,10 +44,10 @@ TEST_CASE("svg::Turtle", "[ut][turtle]")
             ripple_b();
             ripple_c();
         }
-        t.color("black").run(w-offset).turn(0.25).run(hnrw*w*2+w);
+        t.color("black").run(w-offset).turn(0.25).run(hnrw*w+w+ww);
 
         t.up().to(x,y, 0).down();
-        t.color("black").turn(0.25).run(hnrw*w*2+w).turn(-0.25).run(s+offset);
+        t.color("black").turn(0.25).run(hnrw*w+w+ww).turn(-0.25).run(s+offset);
         for (int i = 0; i < nr; ++i)
         {
             ripple_a();
@@ -60,21 +62,61 @@ TEST_CASE("svg::Turtle", "[ut][turtle]")
             t.run(w).down().run(w).turn(0.25).run(d).turn(0.25).run(w).turn(0.25).run(d).turn(0.25).up().run(w);
         }
 
-        t.up().to(x,y, 0).run(h+d).turn(0.25);
+        t.up().to(x,y, 0).run(h+d).turn(0.25).run(ww);
         for (int i = 0; i < hnrw; ++i)
         {
-            t.run(w).down().run(w).turn(0.25).run(d).turn(0.25).run(w).turn(0.25).run(d).turn(0.25).up().run(w);
+            t.run(ww).down().run(ww).turn(0.25).run(d).turn(0.25).run(ww).turn(0.25).run(d).turn(0.25).up().run(ww);
         }
 
-        t.up().to(x,y, 0).run(nr*w*2+w-h).turn(0.25);
+        t.up().to(x,y, 0).run(nr*w*2+w-h).turn(0.25).run(ww);
         for (int i = 0; i < hnrw; ++i)
         {
-            t.run(w).down().run(w).turn(0.25).run(d).turn(0.25).run(w).turn(0.25).run(d).turn(0.25).up().run(w);
+            t.run(ww).down().run(ww).turn(0.25).run(d).turn(0.25).run(ww).turn(0.25).run(d).turn(0.25).up().run(ww);
         }
     };
 
     side(true);
-    side(false);
+    /* side(false); */
+
+    auto front = [&](){
+
+        t.up().to(0,0, 0);
+
+        t.turn(0.25);
+        t.down().run(w-2*d);
+        for (int i = 0; i < hnrw; ++i)
+            t.turn(0.25).run(ww).turn(-0.25).run(ww).turn(-0.25).run(ww).turn(0.25).run(ww);
+        t.run(ww+8);
+        t.turn(-0.25).run(l);
+
+        t.up().to(0,0, 0);
+        t.down().run(l);
+        t.turn(0.25);
+        t.run(w-2*d);
+        for (int i = 0; i < hnrw; ++i)
+            t.turn(-0.25).run(ww).turn(0.25).run(ww).turn(0.25).run(ww).turn(-0.25).run(ww);
+        t.run(ww+8);
+    };
+
+    front();
+
+    auto floor = [&](){
+        t.up().to(0,100, 0);
+        t.down();
+        for (int i = 0; i < nr; ++i)
+            t.run(w).turn(-0.25).run(8).turn(0.25).run(w).turn(0.25).run(8).turn(-0.25);
+        t.run(w);
+        t.turn(0.25).run(l);
+
+        t.up().to(0,100, 0);
+        t.down();
+        t.turn(0.25).run(l).turn(-0.25);
+        for (int i = 0; i < nr; ++i)
+            t.run(w).turn(0.25).run(8).turn(-0.25).run(w).turn(-0.25).run(8).turn(0.25);
+        t.run(w);
+    };
+
+    floor();
 
     std::ofstream fo("turtle.svg");
     REQUIRE(t.stream(fo, 800,600));
