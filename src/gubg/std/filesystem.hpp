62b9c64d615a4f5a48ba9549c2043a733da932a3
Thomas Fannes <thomas.fannes@auro-technologies.com>
2018-01-12 11:43:04 +0100

    Added std::filesystem::exists

--- src/gubg/std/filesystem.hpp
+++ src/gubg/std/filesystem.hpp
@@ -13,6 +13,7 @@ namespace std { namespace filesystem {
     inline path current_path() {return std::experimental::filesystem::current_path();}
     inline bool is_directory(const path &p) {return std::experimental::filesystem::is_directory(p);}
     inline bool is_regular_file(const path &p) {return std::experimental::filesystem::is_regular_file(p);}
+    inline bool exists(const path & p) { return std::experimental::filesystem::exists(p); }
     inline bool create_directory(const path &p) {return std::experimental::filesystem::create_directory(p);}
     inline bool remove(const path &p) {return std::experimental::filesystem::remove(p);}
     inline bool remove_all(const path &p) {return std::experimental::filesystem::remove_all(p);}
