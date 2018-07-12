7a7c993280e1159697b32f74b554be26edd9154b
Geert Fannes <geert.fannes@gmail.com>
2018-07-12 06:35:38 +0200

    Added start of naft::Reader

---
+++ src/gubg/naft/Reader.hpp
@@ -0,0 +1,14 @@
+#ifndef HEADER_gubg_naft_Reader_hpp_ALREADY_INCLUDED
+#define HEADER_gubg_naft_Reader_hpp_ALREADY_INCLUDED
+
+namespace gubg { namespace naft { 
+
+    class Reader
+    {
+    public:
+    private:
+    };
+
+} } 
+
+#endif
