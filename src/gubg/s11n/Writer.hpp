205e45bcd5c38fe44356ea056ec50fd5e8358600
Geert Fannes <geert.fannes@gmail.com>
2018-07-29 23:51:03 +0200

    [dev] s11n::Writer seems to be working

--- src/gubg/s11n/Writer.hpp
+++ src/gubg/s11n/Writer.hpp
@@ -23,7 +23,25 @@ namespace gubg { namespace s11n {
                     return ReturnCode::NoSpaceLeft;
             return ReturnCode::OK;
         }
+        template <typename String>
+        ReturnCode append(String &dst, const std::string &str)
+        {
+            for (const auto ch: str)
+                if (!dst.push_back(ch))
+                    return ReturnCode::NoSpaceLeft;
+            return ReturnCode::OK;
+        }
+        template <typename String, typename Number>
+        ReturnCode append(String &dst, Number number)
+        {
+            return append(dst, std::to_string(number));
+        }
     } 
+
+    enum class State
+    {
+        Attr, Open, Closed, 
+    };
 
     template <typename Tag_, typename String_>
     class Writer
@@ -45,13 +63,106 @@ namespace gubg { namespace s11n {
         ReturnCode named(const Obj &obj, const Name &name)
         {
             MSS_BEGIN(ReturnCode);
+
+            MSS(open_());
+
             MSS(details::push_back(str_, '['));
             MSS(details::append(str_, name));
             MSS(details::push_back(str_, ']'));
+
+            path_.emplace_back();
+            MSS(obj.write(*this));
+            MSS(close_());
+            path_.pop_back();
+
+            MSS_END();
+        }
+        template <typename Obj, typename Name, typename Ftor>
+        ReturnCode named(const Obj &obj, const Name &name, Ftor &&ftor)
+        {
+            MSS_BEGIN(ReturnCode);
+
+            MSS(open_());
+
+            MSS(details::push_back(str_, '['));
+            MSS(details::append(str_, name));
+            MSS(details::push_back(str_, ']'));
+
+            path_.emplace_back();
+            MSS(ftor(*this, obj));
+            MSS(close_());
+            path_.pop_back();
+
+            MSS_END();
+        }
+        
+        template <typename Key, typename Value>
+        ReturnCode attr(const Key &key, const Value &value)
+        {
+            MSS_BEGIN(ReturnCode);
+
+            MSS(!path_.empty());
+            const auto &info = path_.back();
+
+            MSS(info.state == State::Attr);
+
+            MSS(details::push_back(str_, '('));
+            MSS(details::append(str_, key));
+            MSS(details::push_back(str_, ':'));
+            MSS(details::append(str_, value));
+            MSS(details::push_back(str_, ')'));
+
+            MSS_END();
+        }
+        
+        template <typename Value>
+        ReturnCode text(const Value &value)
+        {
+            MSS_BEGIN(ReturnCode);
+
+            MSS(!path_.empty());
+            const auto &info = path_.back();
+
+            MSS(open_());
+
+            MSS(details::append(str_, value));
+
             MSS_END();
         }
 
     private:
+        ReturnCode open_()
+        {
+            MSS_BEGIN(ReturnCode);
+            if (!path_.empty())
+            {
+                auto &info = path_.back();
+                if (info.state == State::Attr)
+                {
+                    info.state = State::Open;
+                    MSS(details::push_back(str_, '{'));
+                }
+            }
+            MSS_END();
+        }
+        ReturnCode close_()
+        {
+            MSS_BEGIN(ReturnCode);
+            MSS(!path_.empty());
+            auto &info = path_.back();
+            if (info.state == State::Open)
+            {
+                info.state = State::Closed;
+                MSS(details::push_back(str_, '}'));
+            }
+            MSS_END();
+        }
+        struct Info
+        {
+            State state = State::Attr;
+        };
+        using Path = std::vector<Info>;
+        Path path_;
         StringAdapter<String> str_;
     };
 
--- test/src/gubg/s11n/Writer_tests.cpp
+++ test/src/gubg/s11n/Writer_tests.cpp
@@ -27,10 +27,42 @@ namespace  {
         std::string str_;
     };
 
-    class A
+    class Array
     {
     public:
+        void push_back(int v) {values_.push_back(v);}
+
+        template <typename Writer>
+        bool write(Writer &writer) const
+        {
+            MSS_BEGIN(bool);
+            MSS(writer.attr("size", values_.size()));
+            size_t ix = 0;
+            for (const auto &value: values_)
+            {
+                MSS(writer.named(value, ix, [](Writer &writer, int value){return writer.attr("value", value);}));
+                ++ix;
+            }
+            MSS_END();
+        }
     private:
+        std::vector<int> values_;
+    };
+    class Text
+    {
+    public:
+        void set(const std::string &str) {str_ = str;}
+
+        template <typename Writer>
+        bool write(Writer &writer) const
+        {
+            MSS_BEGIN(bool);
+            MSS(writer.attr("encoding", "as_is"));
+            MSS(writer.text(str_));
+            MSS_END();
+        }
+    private:
+        std::string str_;
     };
 
     struct MyTag { };
@@ -43,8 +75,9 @@ TEST_CASE("s11n::Writer tests", "[ut][s11n][Writer]")
     struct Scn
     {
         size_t str_size = 0;
-        std::optional<A> a;
-        bool a_as_typed = true;
+        std::optional<Array> array;
+        std::optional<Text> text;
+        bool as_typed = true;
     };
     struct Exp
     {
@@ -57,34 +90,76 @@ TEST_CASE("s11n::Writer tests", "[ut][s11n][Writer]")
     SECTION("default")
     {
     }
-    SECTION("A")
+    SECTION("Array")
     {
-        scn.a.emplace();
+        scn.array.emplace();
         SECTION("as typed")
         {
-            scn.a_as_typed = true;
+            scn.as_typed = true;
         }
         SECTION("as named")
         {
-            scn.str_size += 3;
-            scn.a_as_typed = false;
-            exp.output = "[a]";
+            scn.as_typed = false;
+            SECTION("empty, as named")
+            {
+                exp.output = "[array](size:0)";
+            }
+            SECTION("size 3, as named")
+            {
+                scn.array->push_back(0);
+                scn.array->push_back(1);
+                scn.array->push_back(2);
+                exp.output = "[array](size:3){[0](value:0)[1](value:1)[2](value:2)}";
+            }
         }
     }
+    SECTION("Text")
+    {
+        scn.text.emplace();
+        SECTION("as typed")
+        {
+            scn.as_typed = true;
+        }
+        SECTION("as named")
+        {
+            scn.as_typed = false;
+            SECTION("empty, as named")
+            {
+                exp.output = "[text](encoding:as_is){}";
+            }
+            SECTION("size 3, as named")
+            {
+                scn.text->set("abc");
+                exp.output = "[text](encoding:as_is){abc}";
+            }
+        }
+    }
+    scn.str_size = exp.output.size();
 
     String str(scn.str_size);
     Writer writer(str);
 
     RC rc = RC::OK;
-    if (scn.a)
+    if (scn.array)
     {
-        if (scn.a_as_typed)
+        if (scn.as_typed)
         {
-            AGG(rc, writer.typed(*scn.a));
+            AGG(rc, writer.typed(*scn.array));
         }
         else
         {
-            AGG(rc, writer.named(*scn.a, "a"));
+            AGG(rc, writer.named(*scn.array, "array"));
+        }
+    }
+    if (scn.text)
+    {
+        if (scn.as_typed)
+        {
+            AGG(rc, writer.typed(*scn.text));
+        }
+        else
+        {
+            AGG(rc, writer.named(*scn.text, "text"));
         }
     }
     REQUIRE(exp.rc == rc);
