82c66907a330c10c84426b9a8f02692fd8b4c4b5
Geert Fannes <geert.fannes@gmail.com>
2018-08-07 00:17:36 +0200

    Added start of reader

---
+++ src/gubg/s11n/Reader.hpp
@@ -0,0 +1,14 @@
+#ifndef HEADER_gubg_s11n_Reader_hpp_ALREADY_INCLUDED
+#define HEADER_gubg_s11n_Reader_hpp_ALREADY_INCLUDED
+
+namespace gubg { namespace s11n { 
+
+    class Reader
+    {
+    public:
+    private:
+    };
+
+} } 
+
+#endif
---
+++ test/src/gubg/s11n/Reader_tests.cpp
@@ -0,0 +1,7 @@
+#include "catch.hpp"
+#include "gubg/s11n/Reader.hpp"
+
+TEST_CASE("s11n::Reader tests", "[ut][s11n][Reader]")
+{
+    REQUIRE(false);
+}
