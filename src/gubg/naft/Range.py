83d1ea3ee3155728a68a475de2bdf087b50b1aba
Geert Fannes <geert.fannes@gmail.com>
2019-02-01 19:04:28 +0100

    wip

--- src/gubg/naft/Range.py
+++ src/gubg/naft/Range.py
@@ -1,11 +1,12 @@
-from gubg.string import Range as Strange
+from gubg import string
 
 class Range:
     def __init__(self, str):
-        self.strange = Strange(str)
+        self.strange = string.Range(str)
     def pop_node(self):
         return self.strange.pop_open_close("[]")
     def __repr__(self):
         return f"[naft:Range]{{{str(self.strange)}}}"
     def __str__(self):
         return str(self.strange)
+
