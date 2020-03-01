faa73e53cb0648d35dd8d507ba436f3887bb2061
Geert Fannes <geert.fannes@gmail.com>
2020-03-01 18:03:23 +0100

    wip on wrap encoder

--- src/gubg/wrap/Encoder.hpp
+++ src/gubg/wrap/Encoder.hpp
@@ -2,72 +2,178 @@
 #define HEADER_gubg_wrap_Encoder_hpp_ALREADY_INCLUDED
 
 #include <gubg/wrap/types.hpp>
-#include <gubg/wrap/support/vlc.hpp>
 #include <gubg/Range.hpp>
 #include <gubg/mss.hpp>
 #include <vector>
 #include <algorithm>
 #include <cstdint>
+#include <set>
 
 namespace gubg { namespace wrap { 
+
+    //PDU = [SOM | vlc6(FLIPS)] | flipped(vlc7(SIZE) | SDU)
 
     class Encoder
     {
     public:
-        void operator()(PDU &pdu, const SDU &sdu)
+        Encoder(const std::string &som = ""): som_(som)
         {
-            prepare_header_(sdu);
+        }
 
-            prepare_data_(sdu);
+        bool operator()(PDU &pdu, const SDU &sdu)
+        {
+            MSS_BEGIN(bool);
 
-            pdu.resize(header_.size()+data_.size());
+            MSS(prepare_som_());
+
+            compute_raw_size_sdu_(sdu);
+
+            compute_flips_(sdu);
+
+            pdu.resize(som_.size()+flips_.size()+size_sdu_.size());
 
             auto dst = pdu.begin();
-            std::copy(RANGE(header_), dst);
-            dst += header_.size();
-            std::copy(RANGE(data_), dst);
+            auto copy = [&](const auto &src)
+            {
+                std::copy(RANGE(src), dst);
+                dst += src.size();
+            };
+            copy(som_);
+            copy(flips_);
+            copy(size_sdu_);
+
+            MSS_END();
         }
 
     private:
-        void prepare_header_(const SDU &sdu)
+        bool prepare_som_()
         {
-            header_.resize(0);
+            MSS_BEGIN(bool);
 
-            header_.push_back(0xdc);
-            header_.push_back(0xd1);
+            if (som_is_prepared_)
+                return true;
 
-            support::append_vlc(header_, sdu.size());
+            const auto size = som_.size();
+            if (size > 0)
+            {
+                //SOM without last byte
+                som_start_ = som_;
+                som_start_.resize(size-1);
 
-            compute_flips_(sdu);
-            support::append_vlc(header_, flips_);
+                //Original and flipped version of the last byte
+                som_last_orig_ = som_[size-1];
+                som_last_flipped_ = ~som_last_orig_;
+
+                //Check that all som bytes and the flipped version of the last byte are different
+                {
+                    std::set<char> byte__presence;
+                    for (auto ch: som_)
+                        byte__presence.insert(ch);
+                    byte__presence.insert(som_last_flipped_);
+
+                    MSS(byte__presence.size() == som_.size()+1, std::cout << "Error: SOM characters should be all different, as should be the inverse of the last." << std::endl);
+                }
+            }
+            som_is_prepared_ = true;
+
+            MSS_END();
         }
-        void prepare_data_(const SDU &sdu)
+
+        void append_size_as_vlc7_(std::size_t size, bool is_first = true)
         {
-            data_.resize(sdu.size());
-            //TODO: take the flips into account
-            std::copy(RANGE(sdu), data_.begin());
+            //We recurse before we write our part: big endian at uint16 level
+            if (size > 0x7f)
+                append_size_as_vlc7_(size>>7, false);
+
+            size_sdu_.push_back(is_first ? 0x80 : 0x00);
+            size_sdu_.back() |= (size & 0x7f);
         }
+        void compute_raw_size_sdu_(const SDU &sdu)
+        {
+            size_sdu_.resize(0);
+
+            append_size_as_vlc7_(sdu.size());
+
+            const auto sdu_begin_ix = size_sdu_.size();
+            size_sdu_.resize(sdu_begin_ix+sdu.size());
+
+            std::copy(RANGE(sdu), &size_sdu_[sdu_begin_ix]);
+        }
+
         void compute_flips_(const SDU &sdu)
         {
+            flip_bits_.resize(0);
             flips_.resize(0);
-            for (auto i = 0u; i < sdu.size()/2; ++i)
+
+            if (som_.empty())
+                //User wants no SOM, we will also not include flips_ in the PDU
+                return;
+
+            //Compute flip_bits_, only when the som actually fits in the size_sdu_
+            if (som_.size() <= size_sdu_.size())
             {
-                const auto ix = 2*i;
-                const std::uint16_t msb = static_cast<std::uint16_t>(sdu[ix]  ) << 8;
-                const std::uint16_t lsb = static_cast<std::uint16_t>(sdu[ix+1]);
-                const std::uint16_t part16 = msb|lsb;
-                switch (part16)
+                const std::size_t end_ix = size_sdu_.size()-som_.size()+1;
+                const auto som_start_size = som_start_.size();
+                auto som_start_match_at = [&](std::size_t offset)
                 {
-                    case 0xdcd0: flips_.push_back(false); break;
-                    case 0xdcd1: flips_.push_back(true); break;
-                    default: break;
+                    for (auto ix = 0u; ix < som_start_size; ++ix)
+                        if (som_start_[ix] != size_sdu_[offset+ix])
+                            return false;
+                    return true;
+                };
+                for (auto ix = 0u; ix < end_ix; ++ix)
+                    if (som_start_match_at(ix))
+                    {
+                        auto &last_char = size_sdu_[ix+som_start_size];
+                        if (false) {}
+                        else if (last_char == som_last_orig_)
+                        {
+                            //Flip the last byte and indicate we had to flip it
+                            flip_bits_.push_back(true);
+                            last_char = som_last_flipped_;
+                        }
+                        else if (last_char == som_last_flipped_)
+                        {
+                            //Indicate that we have a false-positive match and that
+                            //the decoder should not flip this last byte
+                            flip_bits_.push_back(false);
+                        }
+                    }
+            }
+
+            //Convert the flip_bits_ into the vlc6-coded flips_
+            {
+                //We initialize the msbit with the msbit of the inverse of som_[0]
+                //to ensure there cannot be a match with som_ in flips_
+                const char init = (~som_[0])&0b10000000;
+
+                for (auto ix = 0u; ix < flip_bits_.size(); ++ix)
+                {
+                    const auto mod = ix%6;
+                    if (mod == 0)
+                        //Add new byte to flips_
+                        flips_.push_back(init);
+                    if (flip_bits_[ix])
+                        //Indicate that we flipped here
+                        flips_.back() |= (1<<mod);
                 }
+                if (flips_.empty())
+                    flips_.push_back(init);
+                //Indicate this was the last byte
+                flips_.back() |= 0b01000000;
             }
         }
 
-        std::vector<std::uint8_t> header_;
-        std::vector<std::uint8_t> data_;
-        std::vector<bool> flips_;
+        const std::string som_;
+        bool som_is_prepared_ = false;
+        std::string som_start_;
+        char som_last_orig_;
+        char som_last_flipped_;
+
+        std::string flips_;
+        std::string size_sdu_;
+
+        std::vector<bool> flip_bits_;
     };
 
 } } 
--- test/src/gubg/wrap/Encoder_tests.cpp
+++ test/src/gubg/wrap/Encoder_tests.cpp
@@ -7,19 +7,43 @@ using namespace gubg;
 
 TEST_CASE("wrap::Encoder tests", "[wrap][Encoder]")
 {
-    wrap::SDU sdu;
+    struct Scn
+    {
+        std::string som;
+        wrap::SDU sdu;
+    };
 
-    SECTION("empty") { }
-    SECTION("abc") { sdu = "abc"; }
+    Scn scn;
 
-    wrap::Encoder encoder;
+    SECTION("default") { }
+    SECTION("som = SOM")
+    {
+        scn.som = "SOM";
+        SECTION("data = ABC") { scn.sdu = "ABC"; }
+        SECTION("data = SOM") { scn.sdu = "SOM"; }
+        SECTION("data = SOMSOmSOM") { scn.sdu = "SOMSO\xb2SOM"; }
+        SECTION("data = SOmSOMSOm") { scn.sdu = "SO\xb2SOMSO\xb2"; }
+    }
+
+    wrap::Encoder encoder{scn.som};
 
     wrap::PDU pdu;
-    encoder(pdu, sdu);
+    encoder(pdu, scn.sdu);
 
     for (auto ch: pdu)
-        std::cout << hex(ch) << ' ';
+        std::cout << hex(ch) << '-' << ch << ' ';
     std::cout << std::endl;
 
-    REQUIRE(pdu.size() >= 6);
+    REQUIRE(pdu.size() >= scn.som.size()+scn.sdu.size());
+    if (!scn.som.empty())
+    {
+        const auto som_size = scn.som.size();
+        std::size_t ix = 0;
+        REQUIRE(pdu.substr(ix, som_size) == scn.som);
+        const auto end_ix = pdu.size()-scn.som.size()+1;
+        for (++ix; ix < end_ix; ++ix)
+        {
+            REQUIRE(pdu.substr(ix, som_size) != scn.som);
+        }
+    }
 }
