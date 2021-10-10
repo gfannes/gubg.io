be3212520e7005a53c9390bc272874b2586e4a6b
Geert Fannes <geert.fannes@gmail.com>
2021-10-10 12:05:29 +0200

    Added termcolor

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,13 @@
+require("gubg/shared")
+
 task :run
 task :proper
 task :clean
-task :prepare
+
+task :prepare do
+	Dir.chdir(GUBG.mkdir("extern")) do
+		GUBG.git_clone('https://github.com/ikalnytskyi', 'termcolor') do
+		end
+	end
+end
+
